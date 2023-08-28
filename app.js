c239415ea641073c4b20a044dddd7fec80f778af
Tydev-js <Tydev.js@gmail.com>
2023-08-28 00:53:17 -0400

    Added ability to add book to log

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const path = require('path');
-
+const Book = require('./models/book')
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/book-logger')
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Home')
 })
 
-app.get('/books', (req, res) => {
-    res.render('index')
+app.get('/books', async (req, res) => {
+    const books = await Book.find({});
+    res.render('index', { books })
+});
+
+app.post('/books', async(req, res) => {
+    const book = new Book(req.body.book)
+    await book.save();
+    res.redirect('/books');
+
 })
 
 app.get('/books/new', (req, res) => {
